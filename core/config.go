d8086ff93ad94897078fa166c3af37e0f7009fe8
Lin, Yong Xiang <r888800009@gmail.com>
2024-09-06 21:37:57 +0800

    feat: Implement file duplication handling in SourceToTargetDedupeStrategy
    This commit implements the file duplication handling in the SourceToTargetDedupeStrategy. It adds the ListFiles function to list all files in a directory, and the duplicateHandler function to handle duplicate files by moving them to the trash directory and optionally replacing them with symlinks. The Execute method of the SourceToTargetDedupeStrategy now calls these functions to identify and handle duplicate files.
    
    Implemented the DirEntry structure regex matching function

--- core/config.go
+++ core/config.go
@@ -3,19 +3,20 @@ package file_cleaner
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
 )
 
-/*
-Strategy defines the what to do with the file
-*/
-type Strategy interface {
-	Load(name string, value map[string]interface{}) error
-}
-
 type DirEntry struct {
-	path        string
-	recursively bool
+	path         string
+	recursively  bool
+	ignore_regex *regexp.Regexp
+	match_regex  *regexp.Regexp
 }
 
 type StrategyConfig struct {
@@ -25,9 +26,10 @@ type StrategyConfig struct {
 }
 
 type SourceToTargetDedupeStrategy struct {
-	super  StrategyConfig
-	target DirEntry
-	source []DirEntry
+	super     StrategyConfig
+	target    DirEntry
+	source    []DirEntry
+	trashPath string
 }
 
 type Config struct {
@@ -37,29 +39,87 @@ type Config struct {
 
 // print dir entry
 func (dir *DirEntry) Print() {
-	println("Path:", dir.path, "Recursively:", dir.recursively)
+	fmt.Println("Path:", dir.path, "Recursively:", dir.recursively)
 }
 
 // Load a strategy entry
 func (config *SourceToTargetDedupeStrategy) Load(name string, value map[string]interface{}) error {
 	config.super.name = name
 	config.super.strategy = value["strategy"].(string)
-	println("Strategy:", config.super.strategy)
+	fmt.Println("Strategy:", config.super.strategy)
 
-	config.target.path = value["target_dir"].(string)
-	config.target.recursively = value["target_dir_recursive"].(bool)
+	config.target.Load(value["target_dir"].(map[string]interface{}))
 	config.target.Print()
+
+	config.trashPath = value["trash_dir"].(string)
+	config.trashPath = expandDir(config.trashPath)
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("2006-01-02-15-04-05.000")
+	config.trashPath = filepath.Join(config.trashPath, formattedTime)
+	fmt.Println("Trash Path:", config.trashPath)
 
 	// Load source directories
 	sourceDirs := value["source_dirs"].([]interface{})
 	for _, sourceDir := range sourceDirs {
 		dir := DirEntry{}
-		dir.path = sourceDir.(map[string]interface{})["path"].(string)
-		dir.recursively = sourceDir.(map[string]interface{})["recursive"].(bool)
+		dir.Load(sourceDir.(map[string]interface{}))
 		dir.Print()
 		config.source = append(config.source, dir)
 	}
 	return nil
+}
+
+/*
+this function expands the directory path
+supports ~ and ~/ expansion
+*/
+func expandDir(path string) string {
+	usr, _ := user.Current()
+	dir := usr.HomeDir
+
+	if path == "~" {
+		return dir
+	} else if strings.HasPrefix(path, "~/") {
+		return filepath.Join(dir, path[2:])
+	}
+	return path
+}
+
+// Load DirEntry
+func (dirEntry *DirEntry) Load(value map[string]interface{}) {
+	dirEntry.path = value["path"].(string)
+	dirEntry.path = expandDir(dirEntry.path)
+
+	dirEntry.recursively = value["recursive"].(bool)
+
+	// load ignore regex if it exists
+	if ignore, ok := value["ignore"]; ok {
+		fmt.Println("Ignore regex:", ignore)
+		dirEntry.ignore_regex = regexp.MustCompile(ignore.(string))
+	} else {
+		fmt.Println("No ignore regex")
+		dirEntry.ignore_regex = nil
+	}
+
+	// load match regex if it exists
+	if match, ok := value["match"]; ok {
+		fmt.Println("Match regex:", match)
+		dirEntry.match_regex = regexp.MustCompile(match.(string))
+	} else {
+		fmt.Println("No match regex")
+		dirEntry.match_regex = nil
+	}
+}
+
+func (dirEntry *DirEntry) Match(path string) bool {
+	result := true
+	if dirEntry.match_regex != nil {
+		result = result && dirEntry.match_regex.MatchString(path)
+	}
+	if dirEntry.ignore_regex != nil {
+		result = result && !dirEntry.ignore_regex.MatchString(path)
+	}
+	return result
 }
 
 // Parse Strategy
@@ -75,7 +135,7 @@ func parseStrategy(key string, value map[string]interface{}) (Strategy, error) {
 		strategy.Load(key, value)
 		return strategy, nil
 	case "pdf_mover":
-		println("Loading pdf_mover strategy")
+		fmt.Println("Loading pdf_mover strategy")
 		return nil, errors.New("pdf_mover strategy not implemented")
 		// strategy := new(PdfMoverStrategy)
 		// strategy.Load(value.(map[string]interface{}))
@@ -93,7 +153,7 @@ func (config_struct *Config) Load(path string) error {
 	}
 
 	config_struct.version = config["version"].(string)
-	println("Config Version:", config_struct.version)
+	fmt.Println("Config Version:", config_struct.version)
 	if config_struct.version != "0.1" {
 		return errors.New("unsupported config version")
 	}
@@ -101,7 +161,7 @@ func (config_struct *Config) Load(path string) error {
 
 	config_struct.strategies = make(map[string]Strategy)
 	for key, jsonValue := range config {
-		println("Found strategy entry:", key)
+		fmt.Println("Found strategy entry:", key)
 		value, ok := jsonValue.(map[string]interface{})
 		if !ok {
 			return errors.New("strategy parse error")
---
+++ core/strategy.go
@@ -0,0 +1,135 @@
+package file_cleaner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// it is the argument for cmd line
+type CmdLineArgs struct {
+	DryRun           bool
+	ReplaceAsSymlink bool
+}
+
+type ExecuteArgs struct {
+	cmd    CmdLineArgs
+	config Config
+}
+
+/*
+Strategy defines the what to do with the file
+*/
+type Strategy interface {
+	Load(name string, value map[string]interface{}) error
+	Execute(parms ExecuteArgs) error
+}
+
+func ListFiles(dirEntry DirEntry) (map[int64]([]FileEntry), map[string]FileEntry) {
+	recursively := dirEntry.recursively
+
+	// list all target files and create a map of size to file, is can chceck quickly if a file exists without reading the file
+	sizeIndex := make(map[int64]([]FileEntry))
+	fileMap := make(map[string]FileEntry)
+	filepath.Walk(dirEntry.path, func(path string, info os.FileInfo, err error) error {
+		// check if file is directory
+		if info.IsDir() && !recursively && path != dirEntry.path {
+			return filepath.SkipDir
+		}
+
+		// check is not match
+		if !dirEntry.Match(path) {
+			return nil
+		}
+
+		entry := FileEntry{}
+		entry.Load(path)
+
+		// make index and map
+		if sizeIndex[entry.size] == nil {
+			sizeIndex[entry.size] = []FileEntry{entry}
+		} else {
+			sizeIndex[entry.size] = append(sizeIndex[entry.size], entry)
+		}
+		fileMap[path] = entry
+
+		return nil
+	})
+
+	return sizeIndex, fileMap
+}
+
+func duplicateHandler(clean FileEntry, keep FileEntry, parms ExecuteArgs, strategy SourceToTargetDedupeStrategy) {
+	fmt.Println("  Duplicate:", clean.path)
+	fmt.Println("    Target:", keep.path)
+
+	// move to trash
+	absPath, _ := filepath.Abs(clean.path)
+	trashPath := filepath.Join(strategy.trashPath, absPath)
+	os.MkdirAll(strategy.trashPath, os.ModePerm)
+	fmt.Println("    Moving to trash:", clean.path)
+	fmt.Println("    Trash Path:", trashPath)
+	if !parms.cmd.DryRun {
+		os.Rename(clean.path, trashPath)
+	} else {
+		fmt.Println("    Dry Run: Not moving to trash")
+	}
+
+	if parms.cmd.ReplaceAsSymlink {
+		fmt.Println("    Replacing with symlink:", clean.path, "->", keep.path)
+		if !parms.cmd.DryRun {
+			os.Symlink(keep.path, clean.path)
+		} else {
+			fmt.Println("    Dry Run: Not creating symlink")
+		}
+		// create symlink
+	}
+
+}
+
+func (strategy *SourceToTargetDedupeStrategy) Execute(parms ExecuteArgs) error {
+	fmt.Println("Execute SourceToTargetDedupeStrategy")
+	fmt.Println("Target:", strategy.target.path)
+
+	// if target directory does not exist, throw an error
+	if _, err := os.Stat(strategy.target.path); os.IsNotExist(err) {
+		return err
+	}
+
+	sizeIndex, fileMap := ListFiles(strategy.target)
+
+	// print all target files
+	for path := range fileMap {
+		fmt.Println("  Target:", path)
+	}
+
+	for _, source := range strategy.source {
+		fmt.Println("Source:", source.path)
+		_, sourceFileMap := ListFiles(source)
+
+		// print all source files
+		for _, entry := range sourceFileMap {
+			// check if file duplicates
+			if targetEntries, ok := sizeIndex[entry.size]; ok {
+				for _, targetEntry := range targetEntries {
+					if entry.Equal(&targetEntry) {
+						duplicateHandler(entry, targetEntry, parms, *strategy)
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (config_struct *Config) Execute(cmdLineArgs CmdLineArgs) error {
+	parms := ExecuteArgs{cmd: cmdLineArgs, config: *config_struct}
+
+	for name, strategy := range config_struct.strategies {
+		fmt.Println("Execute:", name)
+		if err := strategy.Execute(parms); err != nil {
+			return err
+		}
+	}
+	return nil
+}
